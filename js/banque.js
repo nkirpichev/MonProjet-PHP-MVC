51c23a74a387848452601465367eec5179c7a687
Nikolai <nikolai@nikolai>
2023-04-26 19:51:36 +0200

    two

--- js/banque.js
+++ js/banque.js
@@ -3,10 +3,10 @@ function ChangeColor(cl) {
   let testClass = document.getElementsByTagName("a");
   
   for (i = 0; i < testClass.length; i++) {console.log(testClass[i]);
-     if(testClass[i].innerHTML.toLowerCase() == cl.toLowerCase()) testClass[i].classList.add("active");
+     if(testClass[i].innerHTML.toLowerCase() == cl) testClass[i].classList.add("active");
      else testClass[i].classList.remove("active");
      
 }
-  
-  //.style.color = "red";
+ 
+
 }
